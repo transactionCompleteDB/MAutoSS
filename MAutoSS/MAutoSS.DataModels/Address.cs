5cd970eb52c21c5f77273f3c109543c108effe4c
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-01 16:46:55 +0300

    Address db model fully implemented.

--- MAutoSS/MAutoSS.DataModels/Address.cs
+++ MAutoSS/MAutoSS.DataModels/Address.cs
@@ -3,5 +3,11 @@
     public class Address
     {
         public int Id { get; set; }
+
+        public string AddressText { get; set; }
+
+        public int DealershipId { get; set; }
+
+        public virtual Dealership Dealership { get; set; }
     }
 }
