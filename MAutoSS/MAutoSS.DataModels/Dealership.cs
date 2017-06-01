017e3cff702d6fed2b3eb6436e20959253568ad8
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-01 16:46:29 +0300

    Dealership db model fully implemented.

--- MAutoSS/MAutoSS.DataModels/Dealership.cs
+++ MAutoSS/MAutoSS.DataModels/Dealership.cs
@@ -3,5 +3,11 @@
     public class Dealership
     {
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AddressId { get; set; }
+
+        public virtual Address Address { get; set; }
     }
 }
