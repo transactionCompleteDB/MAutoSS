6455dac0e586e3fbecee6bae85a3565d615d82eb
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-01 13:40:10 +0300

    Project DbContext created. Connection string added.

---
+++ MAutoSS/MAutoSS.Data/MAutoSSDbContext.cs
@@ -0,0 +1,31 @@
+﻿using MAutoSS.DataModels;
+using System.Data.Entity;
+
+namespace MAutoSS.Data
+{
+    public class MAutoSSDbContext : DbContext
+    {
+        public MAutoSSDbContext()
+            : base("MAutoSS")
+
+        {
+
+        }
+
+        public virtual IDbSet<Dealership> Dealerships { get; set; }
+
+        public virtual IDbSet<Address> Addresses { get; set; }
+
+        public virtual IDbSet<City> Cities { get; set; }
+
+        public virtual IDbSet<Country> Countries { get; set; }
+
+        public virtual IDbSet<Employee> Employees { get; set; }
+
+        public virtual IDbSet<Sale> Sales { get; set; }
+
+        public virtual IDbSet<Car> Cars { get; set; }
+
+        public virtual IDbSet<CarFeature> CarFeatures { get; set; }
+    }
+}
