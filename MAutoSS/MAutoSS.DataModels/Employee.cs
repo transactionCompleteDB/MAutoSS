8ad100756f61476a76263c7d872c0770adbd50f7
cmdkick <cmdkick@gmail.com>
2017-06-02 13:25:21 +0300

    Employee implemented #5

--- MAutoSS/MAutoSS.DataModels/Employee.cs
+++ MAutoSS/MAutoSS.DataModels/Employee.cs
@@ -1,7 +1,25 @@
-﻿namespace MAutoSS.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MAutoSS.DataModels
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int DealershipId { get; set; }
+
+        [Required]
+        public Dealership Dealership { get; set; }
     }
 }
