b1863fc960c522929f4ca22ff1be6fb014372bbb
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-02 13:11:11 +0300

    GenericRepository and UnitOfWork added.

---
+++ MAutoSS/MAutoSS.Data/Repositories/Contracts/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MAutoSS.Data.Repositories.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+    }
+}
---
+++ MAutoSS/MAutoSS.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+
+using MAutoSS.Data.Repositories.Contracts;
+
+namespace MAutoSS.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext context;
+
+        public UnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Commit()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
