3afba3411fcba25697d8bc4336570a609c55d438
cmdkick <cmdkick@gmail.com>
2017-06-02 12:16:58 +0300

    Fully implemented Car Feature #8

--- MAutoSS/MAutoSS.DataModels/CarFeature.cs
+++ MAutoSS/MAutoSS.DataModels/CarFeature.cs
@@ -1,7 +1,15 @@
-﻿namespace MAutoSS.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MAutoSS.DataModels
 {
     public class CarFeature
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
     }
 }
