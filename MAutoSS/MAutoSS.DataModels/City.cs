293f6afdcefddc70cf5426179ac397974d282847
angel75013 <angelyazarov@yahoo.com>
2017-06-04 20:51:10 +0300

    city+country model

--- MAutoSS/MAutoSS.DataModels/City.cs
+++ MAutoSS/MAutoSS.DataModels/City.cs
@@ -1,7 +1,29 @@
-﻿namespace MAutoSS.DataModels
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MAutoSS.DataModels
 {
     public class City
     {
+        private ICollection<Address> adresses;
+
+        public City()
+        {
+            this.adresses = new HashSet<Address>();
+        }
+
         public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Address> Addresses
+        {
+            get { return this.adresses; }
+            set { this.adresses = value;}
+        }
     }
+
+
 }
--- MAutoSS/MAutoSS.DataModels/Country.cs
+++ MAutoSS/MAutoSS.DataModels/Country.cs
@@ -1,7 +1,31 @@
-﻿namespace MAutoSS.DataModels
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MAutoSS.DataModels
 {
     public class Country
     {
+        private ICollection<City> cities;
+
+        public Country()
+        {
+            this.cities = new HashSet<City>();
+        }
+
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities
+        {
+            get { return this.cities; }
+
+            set { this.cities = value; }
+        }
+
+        
+
     }
 }
