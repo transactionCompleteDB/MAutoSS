398e6eba281ceb9e1aee419ea1ffd1bce7984df9
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-06 15:07:22 +0300

    DealershipInputModel and CreateNewDealership view models added.

---
+++ MAutoSS/MAutoSS.Web/Models/Dealership/DealershipInputModel.cs
@@ -0,0 +1,13 @@
+﻿using MAutoSS.Web.Models.Address;
+using System.ComponentModel.DataAnnotations;
+
+namespace MAutoSS.Web.Models.Dealership
+{
+    public class DealershipInputModel
+    {
+        [Display(Name = "Dealership name")]
+        public string Name { get; set; }
+
+        public AddressViewModel Address { get; set; }
+    }
+}
