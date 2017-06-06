9b7d7e9faac08d048d1abee37b68f1b1eda90942
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-06 20:47:38 +0300

    Ninject installed and fully configured in order to follow dependency inversion.

---
+++ MAutoSS/MAutoSS.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,81 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MAutoSS.Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(MAutoSS.Web.App_Start.NinjectWebCommon), "Stop")]
+
+namespace MAutoSS.Web.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using MAutoSS.Data.Contracts;
+    using MAutoSS.Data;
+    using MAutoSS.Data.Repositories.Contracts;
+    using MAutoSS.Data.Repositories;
+    using MAutoSS.Services;
+    using MAutoSS.Services.Contracts;
+    using System.Data.Entity;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<DbContext>().To<MAutoSSDbContext>().InRequestScope();
+            kernel.Bind<IMAutoSSDbContext>().To<MAutoSSDbContext>().InRequestScope();
+            kernel.Bind(typeof(IGenericRepository<>)).To(typeof(GenericRepository<>)).InRequestScope();
+
+            kernel.Bind<IDealershipService>().To<DealershipService>().InRequestScope();
+
+        }
+    }
+   
+}
