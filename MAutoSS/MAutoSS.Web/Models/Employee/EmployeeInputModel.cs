1466b048bb3d8ec4d681da8c4e9408a88f865f9e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-07 17:01:08 +0300

    EmployeeInputModel added.

---
+++ MAutoSS/MAutoSS.Web/Models/Employee/EmployeeInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MAutoSS.Web.Models.Employee
+{
+    public class EmployeeInputModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DealershipName { get; set; }
+    }
+}
