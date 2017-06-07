bcdefc010ee420092da92936c320e91607247a64
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-07 12:47:47 +0300

    DealershipMainInfoViewModel added.

---
+++ MAutoSS/MAutoSS.Web/Models/Dealership/DealershipMainInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MAutoSS.Web.Models.Address;
+
+namespace MAutoSS.Web.Models.Dealership
+{
+    public class DealershipMainInfoViewModel
+    {
+        public string Name { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+        public int NumberOfCars { get; set; }
+
+        public AddressViewModel Address { get; set; }
+    }
+}
