66b47d6c219c500543982c40fcebed398057760b
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-07 16:59:59 +0300

    EmployeeService added and all methods from IEmployeeService implemented.

---
+++ MAutoSS/MAutoSS.Services/EmployeeService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+using MAutoSS.DataModels;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.Services.Contracts;
+using System;
+
+namespace MAutoSS.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private IGenericRepository<Employee> employeeRepo;
+        private IGenericRepository<Dealership> dealershipRepo;
+        private IDealershipService dealershipService;
+
+        public EmployeeService(
+            IGenericRepository<Employee> employeeRepo, 
+            IGenericRepository<Dealership> dealershipRepo,
+            IDealershipService dealershipService)
+        {
+            this.employeeRepo = employeeRepo;
+            this.dealershipRepo = dealershipRepo;
+            this.dealershipService = dealershipService;
+        }
+
+        IEnumerable<Employee> IEmployeeService.GetAllEmployees()
+        {
+            return this.employeeRepo.GetAll();
+        }
+
+        public void CreateNewEmployee(string firstName, string lastName, string dealershipName)
+        {
+            var dealershipToAssignEmployee = this.dealershipService.GetDealershipIdByName(dealershipName);
+
+            var newEmployee = new Employee
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                DealershipId = dealershipToAssignEmployee.Id
+            };
+
+            this.employeeRepo.Add(newEmployee);
+            this.employeeRepo.SaveChanges();
+        }
+    }
+}
