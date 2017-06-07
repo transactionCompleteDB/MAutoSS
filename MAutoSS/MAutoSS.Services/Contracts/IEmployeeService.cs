f07546038b0e6656a16ee24bf8887762e5d6625c
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-07 16:58:48 +0300

    IEmployeeService added.

---
+++ MAutoSS/MAutoSS.Services/Contracts/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using MAutoSS.DataModels;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services.Contracts
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployees();
+
+        void CreateNewEmployee(string firstName, string lastName, string dealershipName);
+    }
+}
