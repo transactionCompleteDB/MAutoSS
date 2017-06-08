a95535b8fbe18eeff5baed7634ca9b788e483e2a
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-08 10:47:05 +0300

    EmployeeMainInfoModel added.

---
+++ MAutoSS/MAutoSS.Web/Models/Employee/EmployeeMainInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using MAutoSS.Web.Models.Dealership;
+
+namespace MAutoSS.Web.Models.Employee
+{
+    public class EmployeeMainInfoModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Dealership { get; set; }
+
+        public int NumberOfSales { get; set; }
+    }
+}
