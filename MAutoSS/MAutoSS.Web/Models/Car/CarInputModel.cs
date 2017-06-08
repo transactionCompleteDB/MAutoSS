d4f91606ad03113e51311863814e3c8df5bfa54a
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-08 15:37:47 +0300

    CarInputModel and CarFeatureViewModel added.

---
+++ MAutoSS/MAutoSS.Web/Models/Car/CarInputModel.cs
@@ -0,0 +1,27 @@
+﻿using MAutoSS.DataModels.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace MAutoSS.Web.Models.Car
+{
+    public class CarInputModel
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime ManufactureDate { get; set; }
+
+        public int Mileage { get; set; }
+
+        public FuelType FuelType { get; set; }
+
+        public VehicleType VehicleType { get; set; }
+
+        public TransimssionType TransimssionType { get; set; }
+
+        public string Dealership { get; set; }
+
+        public ICollection<CarFeatureViewModel> CarFeatures { get; set; }
+    }
+}
