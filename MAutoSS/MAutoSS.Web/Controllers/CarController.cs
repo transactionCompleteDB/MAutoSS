c2f9eafea411d00e799ce3cb448a55bf685b000a
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-08 13:25:09 +0300

    CarController added.

---
+++ MAutoSS/MAutoSS.Web/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+using Bytes2you.Validation;
+using MAutoSS.Services.Contracts;
+
+namespace MAutoSS.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        private ICarService carService;
+        
+        public CarController(ICarService carService)
+        {
+            Guard.WhenArgument(carService, "carService").IsNull().Throw();
+
+            this.carService = carService;
+        }
+    }
+}
