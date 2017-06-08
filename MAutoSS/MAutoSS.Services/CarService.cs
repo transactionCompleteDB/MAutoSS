62435aae736280f9b9c525ffc6be2247e985324e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-08 13:24:51 +0300

    ICarService.cs and CarService.cs added.

---
+++ MAutoSS/MAutoSS.Services/CarService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.DataModels;
+using MAutoSS.Services.Contracts;
+using Bytes2you.Validation;
+
+namespace MAutoSS.Services
+{
+    public class CarService : ICarService
+    {
+        private IGenericRepository<Car> carRepo;
+
+        public CarService(
+           IGenericRepository<Car> carRepo)
+        {
+            Guard.WhenArgument(carRepo, "carRepo").IsNull().Throw();
+
+            this.carRepo = carRepo;
+        }
+
+        public IEnumerable<Car> GetAllCars()
+        {
+           return this.carRepo.GetAll();
+        }
+
+        public void CreateNewCar(string firstName, string lastName, string dealershipName)
+        {
+            throw new NotImplementedException();
+        }
+
+    
+    }
+}
---
+++ MAutoSS/MAutoSS.Services/Contracts/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using MAutoSS.DataModels;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services.Contracts
+{
+    public interface ICarService
+    {
+        IEnumerable<Car> GetAllCars();
+
+        void CreateNewCar(string firstName, string lastName, string dealershipName);
+    }
+}
