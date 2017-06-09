07b803982e79ed9823c391f14c83d41c3ee72bd7
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-10 02:46:33 +0300

    LoadCarsInfo and MainCarsInfo view models added.

---
+++ MAutoSS/MAutoSS.Web/Models/Car/MainCarsInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace MAutoSS.Web.Models.Car
+{
+    public class MainCarsInfo
+    {
+        public int CarId { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string ManifactureYear { get; set; }
+
+        public string Mileage { get; set; }
+
+        public string Dealership { get; set; }
+    }
+}
