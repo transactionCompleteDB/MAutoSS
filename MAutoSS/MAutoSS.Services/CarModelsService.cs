b2c4bb0ef78679b2b87f0a383ee441dfaf705e0c
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 21:48:16 +0300

    CarModelsService and ICarModelsService added.

---
+++ MAutoSS/MAutoSS.Services/CarModelsService.cs
@@ -0,0 +1,26 @@
+﻿using Bytes2you.Validation;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.DataModels;
+using MAutoSS.Services.Contracts;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services
+{
+    public class CarModelsService : ICarModelsService
+    {
+        private IGenericRepository<CarModel> carModelsRepo;
+
+        public CarModelsService(
+           IGenericRepository<CarModel> carModelsRepo)
+        {
+            Guard.WhenArgument(carModelsRepo, "carModelsRepo").IsNull().Throw();
+
+            this.carModelsRepo = carModelsRepo;
+        }
+
+        public IEnumerable<CarModel> GetAllCarModels()
+        {
+            return this.carModelsRepo.GetAll();
+        }
+    }
+}
