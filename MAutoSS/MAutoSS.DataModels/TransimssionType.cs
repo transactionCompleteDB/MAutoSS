62d4436208024dd0f79da4ec4470994a5675f6e7
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 15:25:24 +0300

    TransimssionType db model added.

---
+++ MAutoSS/MAutoSS.DataModels/TransimssionType.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace MAutoSS.DataModels
+{
+    public class TransimssionType
+    {
+        private ICollection<Car> cars;
+
+        public TransimssionType()
+        {
+            this.cars = new HashSet<Car>();
+        }
+        
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public virtual ICollection<Car> Cars
+        {
+            get { return this.cars; }
+            set { this.cars = value; }
+        }
+    }
+}
