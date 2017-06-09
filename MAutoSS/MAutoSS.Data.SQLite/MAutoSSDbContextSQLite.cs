e74f813ac0d332ee95aae45c7fd939645ce6f9a9
cmdkick <cmdkick@gmail.com>
2017-06-09 14:29:11 +0300

    Adding SQLite context #9

---
+++ MAutoSS/MAutoSS.Data.SQLite/MAutoSSDbContextSQLite.cs
@@ -0,0 +1,15 @@
+﻿using MAutoSS.Data.SQLite.Contracts;
+using MAutoSS.DataModels.SQLite.Models;
+using System.Data.Entity;
+
+namespace MAutoSS.Data.SQLite
+{
+    public class MAutoSSDbContextSQLite : DbContext, IMAutoSSDbContextSQLite
+    {
+        public MAutoSSDbContextSQLite() : base("MAutoSSSQLiteConnection") { }
+
+        public virtual IDbSet<Car> Cars { get; set; }
+
+        public virtual IDbSet<ServiceInfo> ServiceInfos { get; set; }
+    }
+}
