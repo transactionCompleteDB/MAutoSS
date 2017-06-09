9645b5b7142beef5737c81e7f7a582a6233e87f2
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 17:11:59 +0300

    ITransmissionTypesService and TransmissionTypeService added.

---
+++ MAutoSS/MAutoSS.Services/TransmissionTypeService.cs
@@ -0,0 +1,27 @@
+﻿using Bytes2you.Validation;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.DataModels;
+using MAutoSS.Services.Contracts;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services
+{
+    public class TransmissionTypeService : ITransmissionTypesService
+    {
+        private IGenericRepository<TransimssionType> transmissionRepo;
+
+        public TransmissionTypeService(
+           IGenericRepository<TransimssionType> transmissionRepo)
+        {
+            Guard.WhenArgument(transmissionRepo, "transmissionRepo").IsNull().Throw();
+
+            this.transmissionRepo = transmissionRepo;
+        }
+
+        public IEnumerable<TransimssionType> GetAllTransmissions()
+        {
+            return this.transmissionRepo.GetAll();
+        }
+    }
+}
+
