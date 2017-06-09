3f9543fde24d106b471007d56ca4a06d90a10368
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 17:12:26 +0300

    IVehicleTypeService and VehicleTypeService added.

---
+++ MAutoSS/MAutoSS.Services/VehicleTypeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using MAutoSS.DataModels;
+using MAutoSS.Services.Contracts;
+using MAutoSS.Data.Repositories.Contracts;
+using Bytes2you.Validation;
+
+namespace MAutoSS.Services
+{
+    public class VehicleTypeService : IVehicleTypeService
+    {
+        private IGenericRepository<VehicleType> vehicleTypeRepo;
+        private object transmissionRepo;
+
+        public VehicleTypeService(
+           IGenericRepository<VehicleType> vehicleTypeRepo)
+        {
+            Guard.WhenArgument(vehicleTypeRepo, "vehicleTypeRepo").IsNull().Throw();
+
+            this.vehicleTypeRepo = vehicleTypeRepo;
+        }
+
+        public IEnumerable<VehicleType> GetAllVehicleTypes()
+        {
+            return this.vehicleTypeRepo.GetAll();
+        }
+    }
+}
