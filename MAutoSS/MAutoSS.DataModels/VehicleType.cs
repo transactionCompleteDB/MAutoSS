0f6923b342a5d59bff53f65244dafeeb53f59418
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 15:25:39 +0300

    VehicleType db model added.

---
+++ MAutoSS/MAutoSS.DataModels/VehicleType.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace MAutoSS.DataModels
+{
+    public class VehicleType
+    {
+        private ICollection<Car> cars;
+
+        public VehicleType()
+        {
+            this.cars = new HashSet<Car>();
+        }
+
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public virtual ICollection<Car> Cars
+        {
+            get { return this.cars; }
+            set { this.cars = value; }
+        }
+    }
+}
