c8b325f9b5219018db8936149823253fee86dfe3
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-09 17:11:32 +0300

    IFuelTypeService and FuelTypeService added.

---
+++ MAutoSS/MAutoSS.Services/FuelTypeService.cs
@@ -0,0 +1,26 @@
+﻿using MAutoSS.Services.Contracts;
+using System.Collections.Generic;
+using MAutoSS.DataModels;
+using MAutoSS.Data.Repositories.Contracts;
+using Bytes2you.Validation;
+
+namespace MAutoSS.Services
+{
+    public class FuelTypeService : IFuelTypeService
+    {
+        private IGenericRepository<FuelType> fuelTypeRepo;
+
+        public FuelTypeService(
+           IGenericRepository<FuelType> fuelTypeRepo)
+        {
+            Guard.WhenArgument(fuelTypeRepo, "fuelTypeRepo").IsNull().Throw();
+
+            this.fuelTypeRepo = fuelTypeRepo;
+        }
+
+        public IEnumerable<FuelType> GetAllFuelTypes()
+        {
+            return this.fuelTypeRepo.GetAll();
+        }
+    }
+}
