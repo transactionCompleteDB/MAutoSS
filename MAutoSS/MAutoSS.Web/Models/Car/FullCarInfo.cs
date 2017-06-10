1860ce6ec8bb4ad26854e6a246993a9d0a24ca01
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-10 14:01:54 +0300

    FullCarInfo model added to Models.

---
+++ MAutoSS/MAutoSS.Web/Models/Car/FullCarInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace MAutoSS.Web.Models.Car
+{
+    public class FullCarInfo
+    {
+        public int CarId { get; set; }
+        
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+
+        public string VehicleType { get; set; }
+
+        public string FuelType { get; set; }
+
+
+        public string TransmissionType { get; set; }
+
+
+        public string ManufactureYear { get; set; }
+
+
+        public string Mileage { get; set; }
+
+
+        public string Dealership { get; set; }
+
+
+        public ICollection<string> CarFeatures { get; set; }
+    }
+}
