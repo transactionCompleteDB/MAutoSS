f92a00971d67fc6ba88643694f0f81674f4e8469
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-10 23:49:33 +0300

    Required added to Address.

--- MAutoSS/MAutoSS.Web/Models/Address/AddressViewModel.cs
+++ MAutoSS/MAutoSS.Web/Models/Address/AddressViewModel.cs
@@ -5,6 +5,7 @@ namespace MAutoSS.Web.Models.Address
 {
     public class AddressViewModel
     {
+        [Required]
         [Display(Name ="Address")]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Address should be between 1 and 50 chars long")]
         public string AddressText { get; set; }
