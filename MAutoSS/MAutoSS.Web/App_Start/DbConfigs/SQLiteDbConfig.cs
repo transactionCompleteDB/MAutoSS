4cce3669187827e8c0aa73d35c48b61b0e75c6f7
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:23:25 +0300

    SQLiteDbConfig added to DbConfigs in Web.

---
+++ MAutoSS/MAutoSS.Web/App_Start/DbConfigs/SQLiteDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+using MAutoSS.Data.SQLite;
+using MAutoSS.Data.SQLite.Migrations;
+
+namespace MAutoSS.Web.App_Start.DbConfigs
+{
+    public class SQLiteDbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MAutoSSDbContextSQLite, Configuration>());
+        }
+    }
+}
