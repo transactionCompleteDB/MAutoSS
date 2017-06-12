db3c558a05bcd57d32d589809d406db9cc4e5e6e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:11:50 +0300

    PostgreDbConfig created and added to DbConfigs in Web.

---
+++ MAutoSS/MAutoSS.Web/App_Start/DbConfigs/PostgreDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+using MAutoSS.Data.Postgre;
+using MAutoSS.Data.Postgre.Migrations;
+
+namespace MAutoSS.Web.App_Start.DbConfigs
+{
+    public class PostgreDbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MAutoSSDbContextPostgre, Configuration>());
+        }
+    }
+}
