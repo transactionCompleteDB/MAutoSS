cf3206f7e16bf45ecfb391f6b6e98b2f6bb05d82
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:12:25 +0300

    SqlDbConfig created and added to DbConfigs in Web.

---
+++ MAutoSS/MAutoSS.Web/App_Start/DbConfigs/SqlDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+using MAutoSS.Data;
+using MAutoSS.Data.Migrations;
+
+namespace MAutoSS.Web.App_Start.DbConfigs
+{
+    public class SqlDbConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MAutoSSDbContext, Configuration>());
+        }
+    }
+}
