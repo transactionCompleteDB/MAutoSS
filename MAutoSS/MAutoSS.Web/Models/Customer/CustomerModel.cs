af02edfb0dfa1c34cb64de1bd47a55f6c60e21cc
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:17:06 +0300

    Customer Model added.

---
+++ MAutoSS/MAutoSS.Web/Models/Customer/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MAutoSS.Web.Models.Customer
+{
+    public class CustomerModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Discount { get; set; }
+    }
+}
