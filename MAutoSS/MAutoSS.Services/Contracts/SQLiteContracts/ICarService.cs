49f74b70a531657f61f8be08718ecee0aef3bfef
cmdkick <cmdkick@gmail.com>
2017-06-12 12:09:53 +0300

    Completing the ICarService Interface.

--- MAutoSS/MAutoSS.Services/Contracts/SQLiteContracts/ICarService.cs
+++ MAutoSS/MAutoSS.Services/Contracts/SQLiteContracts/ICarService.cs
@@ -5,6 +5,18 @@ namespace MAutoSS.Services.Contracts.SQLiteContracts
 {
     public interface ICarService
     {
+        Car GetCarById(int id);
+
+        Car GetCarByVIN(int VIN);
+
         IEnumerable<Car> GetAllCars();
+
+        void AddNewCar(int VIN);
+
+        void AddServiceDescriptionToCar(int VIN, string serviceInfo);
+
+        IEnumerable<ServiceInfo> GetServiceDescriptionForCar(int VIN);
+
+        void DeleteCar(Car car);
     }
 }
