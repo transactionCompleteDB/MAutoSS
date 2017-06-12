f79c9c9cf3e93bf3db34822d0dc8fa8e2f288c88
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:16:38 +0300

    CustomerController added.

---
+++ MAutoSS/MAutoSS.Web/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using Bytes2you.Validation;
+using MAutoSS.Services.Contracts;
+using MAutoSS.Web.Models.Customer;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MAutoSS.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ICustomerService customerService;
+
+        public CustomerController(
+            ICustomerService customerService)
+        {
+            Guard.WhenArgument(customerService, "customerService").IsNull().Throw();
+
+            this.customerService = customerService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateNewCustomer()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewCustomer(CustomerModel model)
+        {
+            this.customerService.CreateNewCustomer(
+                model.FirstName,
+                model.LastName,
+                decimal.Parse(model.Discount));
+
+            return this.RedirectToAction("LoadCustomersInfo");
+        }
+
+        [HttpGet]
+        public ActionResult LoadCustomersInfo()
+        {
+            var allCustomers = this.customerService
+                .GetAllCustomers()
+                .Select(x => new CustomerModel
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Discount = x.Discount.DiscountPercentage.ToString()
+                });
+
+            return View(allCustomers);
+        }
+    }
+}
