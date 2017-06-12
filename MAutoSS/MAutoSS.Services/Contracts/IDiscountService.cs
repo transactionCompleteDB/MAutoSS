24f345beab59be9294babe477c6e6a1faa58d911
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:16:05 +0300

    IDiscountService and DiscountService added.

---
+++ MAutoSS/MAutoSS.Services/Contracts/IDiscountService.cs
@@ -0,0 +1,12 @@
+﻿using MAutoSS.DataModels.Postgre.Models;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services.Contracts
+{
+    public interface IDiscountService
+    {
+        IEnumerable<Discount> GetAllDiscounts();
+
+        void CreateNewDiscount(decimal discount);
+    }
+}
---
+++ MAutoSS/MAutoSS.Services/DiscountService.cs
@@ -0,0 +1,36 @@
+﻿using Bytes2you.Validation;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.DataModels.Postgre.Models;
+using MAutoSS.Services.Contracts;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private IGenericRepository<Discount> discountsRepo;
+
+        public DiscountService(IGenericRepository<Discount> discountsRepo)
+        {
+            Guard.WhenArgument(discountsRepo, "discountsRepo").IsNull().Throw();
+
+            this.discountsRepo = discountsRepo;
+        }
+
+        public IEnumerable<Discount> GetAllDiscounts()
+        {
+            return this.discountsRepo.GetAll();
+        }
+
+        public void CreateNewDiscount(decimal discount)
+        {
+            var newDiscount = new Discount
+            {
+                DiscountPercentage = discount
+            };
+
+            this.discountsRepo.Add(newDiscount);
+            this.discountsRepo.SaveChanges();
+        }
+    }
+}
