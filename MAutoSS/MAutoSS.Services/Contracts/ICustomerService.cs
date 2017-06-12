2ec2630b135c9ffad4dce7b8ec5af47b13381a1a
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-06-12 11:15:38 +0300

    ICustomerService and CustomerService added.

---
+++ MAutoSS/MAutoSS.Services/Contracts/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using MAutoSS.DataModels.Postgre.Models;
+using System.Collections.Generic;
+
+namespace MAutoSS.Services.Contracts
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAllCustomers();
+
+        void CreateNewCustomer(string firstName, string lastName, decimal discount);
+    }
+}
---
+++ MAutoSS/MAutoSS.Services/CustomerService.cs
@@ -0,0 +1,53 @@
+﻿using Bytes2you.Validation;
+using MAutoSS.Data.Repositories.Contracts;
+using MAutoSS.DataModels.Postgre.Models;
+using MAutoSS.Services.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MAutoSS.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private IGenericRepository<Customer> customersRepo;
+        private IDiscountService discountService;
+
+        public CustomerService(
+            IGenericRepository<Customer> customersRepo,
+            IDiscountService discountService)
+        {
+            Guard.WhenArgument(customersRepo, "customersRepo").IsNull().Throw();
+            Guard.WhenArgument(discountService, "discountService").IsNull().Throw();
+
+            this.customersRepo = customersRepo;
+            this.discountService = discountService;
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return this.customersRepo.GetAll();
+        }
+
+        public void CreateNewCustomer(string firstName, string lastName, decimal discount)
+        {
+            var existingDiscount = this.discountService.GetAllDiscounts().FirstOrDefault(x => x.DiscountPercentage == discount);
+
+            if (existingDiscount == null)
+            {
+                this.discountService.CreateNewDiscount(discount);
+            }
+
+            existingDiscount = this.discountService.GetAllDiscounts().FirstOrDefault(x => x.DiscountPercentage == discount);
+
+            var newCustomer = new Customer
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                DiscountId = existingDiscount.Id
+            };
+
+            this.customersRepo.Add(newCustomer);
+            this.customersRepo.SaveChanges();
+        }
+    }
+}
